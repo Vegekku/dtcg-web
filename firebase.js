4af90997f2c76761462a3e40a68566207f7e83d7
Vegekku <vegekku@gmail.com>
2022-11-27 16:19:11 +0100

    Cambio a sets de cartas

--- firebase.js
+++ firebase.js
@@ -1,48 +1,41 @@
-const st1 = {
-    "name": "ST1",
-    "release": "2022-11-23T18:05:50.094Z",
-    "total": 16,
-    "cards": [
-        {
-            "id": 1,
-            "name": "Koromon"
-        },
-        {
-            "id": 2,
-            "name": "Biyomon"
-        }
-    ]
-};
-
 const sets = [
     {
         "id": "ST1",
         "name": "Starter Deck GAIA RED[ST-1]",
+        "release": null,
+        "total": 16,
+        "url": "bandaitcgplusURL/setID/setID-cardID.png",
+        "alt_cards": {
+            "url": "bandaitcgplusURL/setID/setID-cardID_parallel.png",
+            2: [
+                {
+                    "set": "Event Pack 1",
+                    "parallel": "P1",
+                }
+            ]
+        }
+    },
+    {
+        "id": "ST2",
+        "name": "Starter Deck COCYTUS BLUE[ST-2]",
+        "release": null,
         "total": 16,
         "url": "bandaitcgplusURL/setID/setID-cardID.png"
-    }
-];
-
-const cards = [
-    {
-        "set": "ST1",
-        "card_id": 1,
-        "image": "bandaitcgplusURL/ST1/ST1-01.png",
-        "alter": false,
-        "parallel": false,
     },
     {
-        "set": "ST1",
-        "card_id": 2,
-        "image": "bandaitcgplusURL/ST1/ST1-02.png",
-        "alter": false,
-        "parallel": false,
-    },
-    {
-        "set": "Event Pack 1",
-        "card_id": 2,
-        "image": "bandaitcgplusURL/ST1/ST1-02_P1.png",
-        "alter": false,
-        "parallel": "P1",
+        "id": null,
+        "name": "Event Pack 1",
+        "release": null,
+        "url": "bandaitcgplusURL/setID/setID-cardID_parallel.png",
+        "cards": {
+            "ST1-02": "P1",
+            "ST1-06": "P1",
+            "ST1-11": "P2",
+            "ST2-02": "P1",
+            "ST2-07": "P1",
+            "ST2-11": "P1",
+            "ST3-02": "P1",
+            "ST3-07": "P1",
+        }
     }
 ];
