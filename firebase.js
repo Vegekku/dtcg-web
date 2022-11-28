699c678747c6c3826c1087d26aa31388d2f40ec5
Vegekku <vegekku@gmail.com>
2022-11-28 22:39:58 +0100

    Added more cards

--- firebase.js
+++ firebase.js
@@ -1,4 +1,15 @@
 const sets = [
+    // Promos
+    {
+        "id": "P",
+        "name": "Promos",
+        "release": null,
+        "total": 81,
+        "url": null,
+        "add_zero": 3,
+        "info_url": null
+    },
+    // Starter Deck GAIA RED [ST-1]
     {
         "id": "ST1",
         "name": "Starter Deck GAIA RED [ST-1]",
@@ -8,6 +19,7 @@ const sets = [
         "add_zero": 2,
         "info_url": "https://world.digimoncard.com/products/deck/st-1.php"
     },
+    // Starter Deck COCYTUS BLUE [ST-2]
     {
         "id": "ST2",
         "name": "Starter Deck COCYTUS BLUE [ST-2]",
@@ -17,6 +29,7 @@ const sets = [
         "add_zero": 2,
         "info_url": "https://world.digimoncard.com/products/deck/st-2.php"
     },
+    // Starter Deck HEAVEN'S YELLOW [ST-3]
     {
         "id": "ST3",
         "name": "Starter Deck HEAVEN'S YELLOW [ST-3]",
@@ -26,6 +39,7 @@ const sets = [
         "add_zero": 2,
         "info_url": "https://world.digimoncard.com/products/deck/st-3.php"
     },
+    // Starter Deck GIGA GREEN [ST-4]
     {
         "id": "ST4",
         "name": "Starter Deck GIGA GREEN [ST-4]",
@@ -35,6 +49,7 @@ const sets = [
         "add_zero": 2,
         "info_url": "https://world.digimoncard.com/products/deck/st-4.php"
     },
+    // Starter Deck MACHINE BLACK [ST-5]
     {
         "id": "ST5",
         "name": "Starter Deck MACHINE BLACK [ST-5]",
@@ -44,6 +59,7 @@ const sets = [
         "add_zero": 2,
         "info_url": "https://world.digimoncard.com/products/deck/st-5.php"
     },
+    // Starter Deck VENOMOUS VIOLET [ST-6]
     {
         "id": "ST6",
         "name": "Starter Deck VENOMOUS VIOLET [ST-6]",
@@ -53,6 +69,7 @@ const sets = [
         "add_zero": 2,
         "info_url": "https://world.digimoncard.com/products/deck/st-6.php"
     },
+    // Starter Deck GALLANTMON [ST-7]
     {
         "id": "ST7",
         "name": "Starter Deck GALLANTMON [ST-7]",
@@ -62,6 +79,7 @@ const sets = [
         "add_zero": 2,
         "info_url": "https://world.digimoncard.com/products/deck/st-7.php"
     },
+    // Starter Deck ULFORCEVEEDRAMON [ST-8]
     {
         "id": "ST8",
         "name": "Starter Deck ULFORCEVEEDRAMON [ST-8]",
@@ -71,6 +89,7 @@ const sets = [
         "add_zero": 2,
         "info_url": "https://world.digimoncard.com/products/deck/st-8.php"
     },
+    // Starter Deck ULTIMATE ANCIENT DRAGON [ST-9]
     {
         "id": "ST9",
         "name": "Starter Deck ULTIMATE ANCIENT DRAGON [ST-9]",
@@ -80,6 +99,7 @@ const sets = [
         "add_zero": 2,
         "info_url": "https://world.digimoncard.com/products/deck/st-9.php"
     },
+    // Starter Deck PARALLEL WORLD TACTICIAN [ST-10]
     {
         "id": "ST10",
         "name": "Starter Deck PARALLEL WORLD TACTICIAN [ST-10]",
@@ -89,6 +109,7 @@ const sets = [
         "add_zero": 2,
         "info_url": "https://world.digimoncard.com/products/deck/st-10.php"
     },
+    // Starter Deck JESMON [ST-12]
     {
         "id": "ST12",
         "name": "Starter Deck JESMON [ST-12]",
@@ -98,6 +119,7 @@ const sets = [
         "add_zero": 2,
         "info_url": "https://world.digimoncard.com/products/deck/st-12.php"
     },
+    // Starter Deck RagnaLoardmon [ST-13]
     {
         "id": "ST13",
         "name": "Starter Deck RagnaLoardmon [ST-13]",
@@ -107,6 +129,7 @@ const sets = [
         "add_zero": 2,
         "info_url": "https://world.digimoncard.com/products/deck/st-13.php"
     },
+    // Booster New Evolution [BT1]
     {
         "id": "BT1",
         "name": "Booster New Evolution [BT1]",
@@ -133,6 +156,105 @@ const sets = [
             }
         }
     },
+    // Booster Ultimate Power [BT2]
+    {
+        "id": "BT2",
+        "name": "Booster Ultimate Power [BT2]",
+        "release": null,
+        "total": 112,
+        "url": "bandaitcgplusURL/setID/setID-cardID.png",
+        "add_zero": 3,
+        "alternatives": {
+            "url": "bandaitcgplusURL/setID/setID-cardID_parallel.png",
+            "cards": {
+                // "BT1-025": "P1",
+                // "BT1-044": "P1",
+                // "BT1-060": "P1",
+                // "BT1-082": "P1",
+                // "BT1-084": "P1",
+                // "BT1-085": "P1",
+                // "BT1-086": "P1",
+                // "BT1-087": "P1",
+                // "BT1-088": "P1",
+                // "BT1-089": "P1",
+                // "BT1-110": "P1",
+                // "BT1-114": "P1",
+                // "BT1-115": "P1",
+            }
+        }
+    },
+    // Booster Union Impact [BT3]
+    {
+        "id": "BT3",
+        "name": "Booster Union Impact [BT3]",
+        "release": null,
+        "total": 112,
+        "url": "bandaitcgplusURL/setID/setID-cardID.png",
+        "add_zero": 3,
+        "alternatives": {
+            "url": "bandaitcgplusURL/setID/setID-cardID_parallel.png",
+            "cards": {
+                // "BT1-025": "P1",
+                // "BT1-044": "P1",
+                // "BT1-060": "P1",
+                // "BT1-082": "P1",
+                // "BT1-084": "P1",
+                // "BT1-085": "P1",
+                // "BT1-086": "P1",
+                // "BT1-087": "P1",
+                // "BT1-088": "P1",
+                // "BT1-089": "P1",
+                // "BT1-110": "P1",
+                // "BT1-114": "P1",
+                // "BT1-115": "P1",
+            }
+        }
+    },
+    // Promotion Pack Ver.0.0
+    {
+        "id": null,
+        "name": "Promotion Pack Ver.0.0",
+        "release": null,
+        "url": "bandaitcgplusURL/setID/setID-cardID.png",
+        "cards": {
+            "P-001": "",
+            "P-002": "",
+            "P-003": "",
+            "P-004": "",
+            "P-005": "",
+            "P-006": ""
+        },
+    },
+    // Special Box Promotion Pack
+    {
+        "id": null,
+        "name": "Special Box Promotion Pack",
+        "release": "February 12, 2021",
+        "url": "bandaitcgplusURL/setID/setID-cardID.png",
+        "cards": {
+            "P-007": "",
+            "P-008": "",
+            "P-009": "",
+            "P-010": "",
+            "P-011": "",
+            "P-012": ""
+        },
+    },
+    // Dash Pack Ver. 1.0
+    {
+        "id": null,
+        "name": "Dash Pack Ver. 1.0",
+        "release": "February 12, 2021",
+        "url": "bandaitcgplusURL/setID/setID-cardID_parallel.png",
+        "cards": {
+            "BT1-010": "P1",
+            "BT1-029": "P1",
+            "BT2-030": "P1",
+            "BT2-049": "P1",
+            "BT2-066": "P1"
+        },
+    },
+    // Event Pack 1
     {
         "id": null,
         "name": "Event Pack 1",
