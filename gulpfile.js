1dbef03c539e3be06a209994f6f444964cf6dfc5
Vegekku <vegekku@gmail.com>
2023-04-10 21:25:13 +0200

    Uso de sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,22 @@
 'use strict';
 
 const gulp = require( 'gulp' );
-const sass = require( 'gulp-sass' );
-const sourcemaps = require( 'gulp-sourcemaps' );
+const sass = require( 'gulp-sass' )(require('sass'));
 const autoprefixer = require( 'gulp-autoprefixer' );
 
 function styles() {
 	return gulp
-		.src( `./sass/*.scss` )
-		.pipe( sourcemaps.init() )
-		// .pipe( sass({ outputStyle: 'compressed' }).on( 'error', sass.logError ) )
+		.src( './sass/**/*.scss' )
 		.pipe( sass().on( 'error', sass.logError ) )
 		.pipe( autoprefixer() )
-		.pipe( sourcemaps.write( './' ) )
-		.pipe(
-			gulp.dest( function( file ) {
-				file.dirname = file.dirname.replace( '/sass', '/css' );
-				return file.base;
-			})
-		);
+		.pipe( gulp.dest( './css' ) );
 }
 
 function watch() {
-	gulp.watch( `./sass/*.scss`, gulp.series( styles ) );
+	gulp.watch( './sass/**/*.scss', gulp.series( styles ) );
 }
 
-exports.pluginStyles = pluginStyles;
-exports.themeStyles = themeStyles;
+exports.styles = styles;
 exports.watch = watch;
 
-exports.default = gulp.series( pluginStyles, themeStyles, watch );
+exports.default = gulp.series( styles, watch );
