b70ab92bb73a3dc59f3cfe4b3d5b1bc2efada35d
Vegekku <vegekku@gmail.com>
2023-04-10 17:24:17 +0200

    Listar como tabla/grid y mostrar set/todos

---
+++ views.js
@@ -0,0 +1,15 @@
+const list = () => {
+    const list = document.getElementById('list').value;
+    const setLists = document.getElementById('setLists');
+
+    setLists.classList.remove('view--table','view--grid');
+    setLists.classList.add('view--'+list);
+};
+
+const show = () => {
+    const show = document.getElementById('show').value;
+    const setLists = document.getElementById('setLists');
+
+    setLists.classList.remove('view--set','view--all');
+    setLists.classList.add('view--'+show);
+}
