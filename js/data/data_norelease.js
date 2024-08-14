7a52821519f846e4850f3efe381e6e44b798f246
Vegekku <vegekku@gmail.com>
2024-08-15 00:26:04 +0200

    EX7 y aumento de promos

--- js/data/data_norelease.js
+++ js/data/data_norelease.js
@@ -5,7 +5,7 @@ const dataNoRelease = [
         "id": "P",
         "name": "Promos",
         "release": null,
-        "total": 147,
+        "total": 151,
         "url": null,
         "add_zero": 3,
         "info_url": null,
