fd245bde2e5587bb4b477415d322d0119d776b23
Vegekku <vegekku@gmail.com>
2024-12-18 16:34:46 +0100

    Futuros boosters y starters

--- js/data/data_2025.js
+++ js/data/data_2025.js
@@ -1,4 +1,132 @@
 const data2025 = [
+// January 1 - March 31, 2025
+    // Official Store Tournament 2025 Vol.1 - Participation Pack --- otp16
+    {
+        "id": null,
+        "slug": "otp16",
+        "name": "Official Store Tournament 2025 Vol.1 - Participation Pack",
+        "release": "January 1 - March 31, 2025",
+        "url": "noCardURL",
+        "cards": {
+            "P-164": "",
+            "P-165": "",
+            "P-166": "",
+            "P-167": "",
+            "P-168": "",
+            "P-169": "",
+        },
+        "info_url": "https://world.digimoncard.com/event/2025/store_tournament/01/",
+    },
+    // Official Store Tournament 2025 Vol.1 - Winner Pack --- wp16
+    {
+        "id": null,
+        "slug": "wp16",
+        "name": "Official Store Tournament 2025 Vol.1 - Winner Pack",
+        "release": "January 1 - March 31, 2025",
+        "url": "noCardURL",
+        "cards": {
+            "P-164": "",
+            "P-165": "",
+            "P-166": "",
+            "P-167": "",
+            "P-168": "",
+            "P-169": "",
+        },
+        "info_url": "https://world.digimoncard.com/event/2025/store_tournament/01/",
+    },
+// January 10, 2025
+    // Booster Chain of Liberation [EX8] --- ex8
+    {
+        "id": "EX8",
+        "slug": "ex8",
+        "name": "Booster Chain of Liberation [EX8]",
+        "release": "January 10, 2025",
+        "total": 74,
+        "url": "bandaitcgplusURL/EX08/e_setID-cardID_d.png",
+        "add_zero": 3,
+        // "pack": "https://static.wikia.nocookie.net/digimoncardgame/images/b/b3/BT-15-Booster_Pack-EN.png",
+        "playmat": "",
+        // "color": {
+        //     "red": [1,"7-14",66],
+        //     "blue": [2,"15-23",67],
+        //     "yellow": [3,"24-28",30,63,68],
+        //     "yellow-green": [29],
+        //     "green": [4,"31-36",64,69],
+        //     "green-yellow-black": [37],
+        //     "green-yellow-purple": [74],
+        //     "black": [5,"38-49",70],
+        //     "purple": [6,"51-54",56,"58-61",65,"71-73"],
+        //     "purple-red": [50,55,57,62],
+        // },
+        // "override": {
+        //     "url": "bandaitcgplusURL/EX07/e_setID-cardID.png",
+        //     "cards": {
+        //         "EX7-072": "",
+        //         "EX7-074": ""
+        //     }
+        // },
+        "info_url": "https://world.digimoncard.com/products/pack/ex-08/"
+    },
+    // Booster Chain of Liberation [EX8] - Alternatives --- ex8_alts
+    {
+        "id": null,
+        "slug": "ex8_alts",
+        "name": "Booster Chain of Liberation [EX8] - Alternatives",
+        "release": "January 10, 2025",
+        "url": "bandaitcgplusURL/EX08/e_setID-cardIDp_d.png",
+        "cards": {
+        },
+        "info_url": "https://world.digimoncard.com/products/pack/ex-08/",
+    },
+    // Booster Chain of Liberation [EX8] - Limited Cards --- ex8_limited
+    {
+        "id": null,
+        "slug": "ex8_limited",
+        "name": "Booster Chain of Liberation [EX8] - Limited Cards",
+        "release": "January 10, 2025",
+        "url": "bandaitcgplusURL/EX08/e_setID-cardID_d_l.png",
+        "cards": {
+        },
+        "info_url": "https://world.digimoncard.com/products/pack/ex-08/",
+    },
+    // Booster Chain of Liberation [EX8] - Special Cards --- ex8_special
+    {
+        "id": null,
+        "slug": "ex8_special",
+        "name": "Booster Chain of Liberation [EX8] - Special Cards",
+        "release": "January 10, 2025",
+        "url": "bandaitcgplusURL/EX08/e_setID-cardIDsp_d.png",
+        "cards": {
+        },
+        "info_url": "https://world.digimoncard.com/products/pack/ex-08/",
+    },
+    // Booster Chain of Liberation [EX8] - Legend Pack 2024 --- ex8_legend_pack
+    {
+        "id": null,
+        "slug": "ex8_legend_pack",
+        "name": "Booster Chain of Liberation [EX8] - Legend Pack 2024",
+        "release": "January 10, 2025",
+        "url": "bandaitcgplusURL/EX08/e_setID-cardID_d.png",
+        "cards": {
+        },
+        "info_url": "https://world.digimoncard.com/products/pack/ex-08/",
+    },
+    // Booster Chain of Liberation [EX8] - Upgrade Pack --- ex8_upgrade_pack
+    {
+        "id": null,
+        "slug": "ex8_upgrade_pack",
+        "name": "Booster Chain of Liberation [EX8] - Upgrade Pack",
+        "release": "January 10, 2025",
+        "url": "bandaitcgplusURL/EX08/e_setID-cardID_d.png",
+        "cards": {
+            "P-160": "",
+            "P-161": "",
+            "P-162": "",
+            "P-163": ""
+        },
+        "info_url": "https://world.digimoncard.com/products/pack/ex-08/",
+    },
+
 // February 2025
     // Omnimon Binder Set [PB19] --- pb19
     {
@@ -33,6 +161,69 @@ const data2025 = [
         "info_url": "https://world.digimoncard.com/products/pack/ver19-20/"
     },
 
+// April 2025
+    // Starter Deck Protector of Light [ST20] --- st20
+    {
+        "id": "ST20",
+        "slug": "st20",
+        "name": "Starter Deck Protector of Light [ST-20]",
+        "release": "April 2025",
+        "total": 15,
+        "url": "bandaitcgplusURL/setID/e_setID-cardID_D.png",
+        "add_zero": 2,
+        "info_url": "https://world.digimoncard.com/products/deck/st-20/",
+        "color": {
+            // "green": [1,"3-15"],
+            // "green-red": [2]
+        }
+    },
+    // Starter Deck Hero of Hope [ST21] --- st21
+    {
+        "id": "ST21",
+        "slug": "st21",
+        "name": "Starter Deck Hero of Hope [ST-21]",
+        "release": "April 2025",
+        "total": 15,
+        "url": "bandaitcgplusURL/setID/e_setID-cardID_D.png",
+        "add_zero": 2,
+        "info_url": "https://world.digimoncard.com/products/deck/st-21/",
+        "color": {
+            // "green": [1,"3-15"],
+            // "green-red": [2]
+        }
+    },
+    // Booster World Convergence [BT21] --- bt21
+    {
+        "id": "BT21",
+        "slug": "bt21",
+        "name": "Booster World Convergence [BT21]",
+        "release": "April 2025",
+        "total": 74,
+        "url": "bandaitcgplusURL/EX08/e_setID-cardID_d.png",
+        "add_zero": 3,
+        // "pack": "https://static.wikia.nocookie.net/digimoncardgame/images/b/b3/BT-15-Booster_Pack-EN.png",
+        "playmat": "",
+        // "color": {
+        //     "red": [1,"7-14",66],
+        //     "blue": [2,"15-23",67],
+        //     "yellow": [3,"24-28",30,63,68],
+        //     "yellow-green": [29],
+        //     "green": [4,"31-36",64,69],
+        //     "green-yellow-black": [37],
+        //     "green-yellow-purple": [74],
+        //     "black": [5,"38-49",70],
+        //     "purple": [6,"51-54",56,"58-61",65,"71-73"],
+        //     "purple-red": [50,55,57,62],
+        // },
+        // "override": {
+        //     "url": "bandaitcgplusURL/EX07/e_setID-cardID.png",
+        //     "cards": {
+        //         "EX7-072": "",
+        //         "EX7-074": ""
+        //     }
+        // },
+        "info_url": "https://world.digimoncard.com/products/pack/ver21/"
+    },
 // May 2025
     // DIGIMON ANIMATION SERIES 25th Anniversary Set [PB20] --- pb20
     {
@@ -42,8 +233,38 @@ const data2025 = [
         "release": "May 2025",
         "url": "noCardURL",
         "cards": {
-            // https://wikimon.net/PB-20_(DCG)
+            "BT1-085": "",
+            "BT3-093": "",
+            "BT7-085": "",
+            "BT10-087": "",
+            "BT12-095": "",
+            "BT12-096": "",
+            "BT13-095": "",
+            "EX2-056": "",
+            "RB1-032": "",
+            "P-156": "",
         },
-        "info_url": "https://world.digimoncard.com/products/goods/omnimon-binder-set.php"
+        "info_url": "https://world.digimoncard.com/products/goods/animation_series_25th.php"
+    },
+    // TAMER'S SELECTION BOX ver. CHAMPIONSHIP 2024 --- pb21
+    {
+        "id": null,
+        "slug": "pb21",
+        "name": "TAMER'S SELECTION BOX ver. CHAMPIONSHIP 2024",
+        "release": "May 2025",
+        "url": "noCardURL",
+        "cards": {
+            "BT17-017": "",
+            "BT17-028": "",
+            "BT18-017": "",
+            "BT18-028": "",
+            "BT18-029": "",
+            "BT18-054": "",
+            "BT18-055": "",
+            "BT18-072": "",
+            "BT18-074": "",
+            "BT18-084": "",
+        },
+        "info_url": "https://world.digimoncard.com/products/goods/tamer-selection-box-cs2024.php"
     },
 ];
