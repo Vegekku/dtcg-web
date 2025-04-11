97d912ddd03c9d799b72329ada19e489793ce62a
Vegekku <vegekku@gmail.com>
2025-04-11 12:18:38 +0200

    pb21

--- js/updates.js
+++ js/updates.js
@@ -1,12 +1,22 @@
-const updatesData = () => {
+const updatesData = ( version ) => {
+    // TODO: Mejora de ejecución de script.
     // 1. detectar la versión de los datos.
     // 2. mediante un switch, ejecutar los cambios correspondientes hasta llegar a la versión actual.
     // 3. guardar la última versión de datos.
-    let collectionData = localStorage.getItem('collection');
-    collectionData = collectionData.replace(/"LM01"/g, '"LM"');
-    localStorage.setItem('collection', collectionData);
 
+    let collectionData = localStorage.getItem('collection');
     let cardmarketData = localStorage.getItem('cardmarket');
-    cardmarketData = cardmarketData.replace(/"LM01"/g, '"LM"');
+
+    if ( 'LM01' === version ) {
+        collectionData = collectionData.replace(/"LM01"/g, '"LM"');
+        cardmarketData = cardmarketData.replace(/"LM01"/g, '"LM"');
+    }
+
+    if ( 'pb21' === version ) {
+        collectionData = collectionData.replace(/"pb21"/g, '"tamer_champion_24"');
+        cardmarketData = cardmarketData.replace(/"pb21"/g, '"tamer_champion_24"');
+    }
+    
+    localStorage.setItem('collection', collectionData);
     localStorage.setItem('cardmarket', cardmarketData);
 };
