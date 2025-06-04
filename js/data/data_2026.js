44b270922b45fe9775b9e78fe3cdb980b381a658
Vegekku <vegekku@gmail.com>
2025-06-04 11:53:37 +0200

    Digimon Liberator Debuggers Set

--- js/data/data_2026.js
+++ js/data/data_2026.js
@@ -23,4 +23,42 @@ const data2026 = [
         },
         "info_url": "https://world.digimoncard.com/products/goods/tamers-evolution-box/"
     },
+// February 2026
+    // Digimon Liberator Debuggers Set [pb22] --- pb22
+    {
+        "id": null,
+        "slug": "pb22",
+        "name": "Digimon Liberator Debuggers Set [pb21]",
+        "release": "January 2026",
+        "url": "noCardURL",
+        "cards": {
+            "ST18-12": "",
+            "ST18-14": "",
+            "ST19-14": "",
+            "EX7-030": "",
+            "EX7-062": "",
+            "EX7-065": "",
+            "EX7-074": "",
+            "EX8-016": "",
+            "EX8-028": "",
+            "EX8-055": "",
+            "EX8-065": "",
+            "EX8-066": "",
+            "EX8-067": "",
+            "EX9-033": "",
+            "EX9-067": "",
+            "BT19-027": "",
+            "BT19-053": "",
+            "BT19-082": "",
+            "BT19-084": "",
+            "BT20-055": "",
+            "BT20-079": "",
+            "BT20-086": "",
+            "BT20-088": "",
+            "BT21-029": "",
+            "BT21-081": "",
+            "P-151": "",
+        },
+        "info_url": "https://world.digimoncard.com/products/goods/liberator-debuggers-set.php"
+    },
 ];
