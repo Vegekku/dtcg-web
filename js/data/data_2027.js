b219cf9dee87a65c3074aa75b84b2a8d2fc5798f
Vegekku <vegekku@gmail.com>
2026-06-05 10:49:43 +0200

    data: add PB-24 Nexus of Digimon and create data_2027.js

---
+++ js/data/data_2027.js
@@ -0,0 +1,29 @@
+const data2027 = [
+// January 2027
+    // Tamer's Evolution Card Set - Nexus of Digimon [PB-24] --- pb24
+    {
+        "id": null,
+        "block": {
+            "4": ["EX7"],
+            "5": ["BT23","BT24","EX10","EX11","P"]
+        },
+        "slug": "pb24",
+        "name": "Tamer's Evolution Card Set - Nexus of Digimon [PB-24]",
+        "release": "January 2027",
+        "url": "noCardURL",
+        "cards": {
+            "BT24-034": "",
+            "BT24-040": "",
+            "BT24-030": "",
+            "EX7-059": "",
+            "EX10-074": "",
+            "P-223": "",
+            "EX11-074": "",
+            "EX11-012": "",
+            "BT23-102": "",
+            "BT23-101": "",
+        },
+        "info_url": "https://world.digimoncard.com/products/goods/nexus-of-digimon/",
+        // TODO: también incluye card case, storage box, sleeves y counter markers (pendiente soporte de merchandise)
+    },
+];
